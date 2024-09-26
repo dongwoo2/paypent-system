40a1eaf1ae1c2b2447d3de5fcdf07ddc02f7f14d
dongwoo2 <ieiedong@naver.com>
2024-09-26 16:17:53 +0900

    settings 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     # third apps
     "debug_toolbar",
     "django_bootstrap5",
